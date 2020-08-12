ed8c2436ade7d1692fa59510b4d9ec99b2db23b9
webAqemu <notych2812@gmail.com>
2020-08-12 16:03:45 +0300

    added project

---
+++ app/js/script.js
@@ -0,0 +1,66 @@
+$(function () {
+
+	$('.slider__inner').slick({
+                vertical: true,
+                verticalSwiping: true,
+                dots: true,
+                infinity: true,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                speed: 1000,
+                initialSlide: 1,
+                arrows: false,
+        });
+
+        $('.new__inner').slick({
+                dots: true,
+                infinity: true,
+                arrows: false,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [
+                        {
+                                breakpoint: 1200,
+                                settings: {
+                                        slidesToShow: 3,
+                                        slidesToScroll: 3,
+                                }
+                        },
+                        {
+                                breakpoint: 900,
+                                settings: {
+                                        slidesToShow: 2,
+                                        slidesToScroll: 2,
+                                }
+                        },
+                        {
+                                breakpoint: 621,
+                                settings: {
+                                        slidesToShow: 1,
+                                        slidesToScroll: 1,
+                                        centerMode: true,
+                                        centerPadding: '20px',
+                                }
+                        },
+                      ]
+        });
+
+        $(".best__rate").rateYo({
+                rating: 4.5,
+                normalFill: '#e8ecf3',
+                ratedFill: '#f55d44',
+                numStars: 5,
+                starWidth: "12px",
+                readOnly: true,
+                fullStar: true,
+        });
+
+        $('.header__burger').click(function(event){
+                $('.header__burger, .header__menu').toggleClass('active')
+        });
+
+        $('.header__btn--search').click(function(event){
+                $('.header__input-inner, .header__input').toggleClass('active')
+        });
+ 
+});
